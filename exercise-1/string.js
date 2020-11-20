270f555c287b6af425eb291c249a5a01690393f4
j-mello <mello.julien@gmail.com>
2020-11-20 09:36:06 +0100

    [DONE] exercice 2

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -93,7 +93,6 @@ function verlan(chaine) {
     for (let i=0;i<words.length;i++)
     {
         if(i>0) str += " ";
-
         let verlanWord = "";
         for (let j=words[i].length-1; j>=0; j--)
         {
